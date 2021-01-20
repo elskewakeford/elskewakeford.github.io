4d7d71730b89ec36df1cb13499e8892cdba9da24
Elske Wakeford <69579529+octothorp-elske@users.noreply.github.com>
2021-01-20 12:19:50 +0200

    New

---
+++ Buttons/index.js
@@ -0,0 +1,22 @@
+for (let i = 1; i <= 100; i++) {
+    const newButton = document.createElement('button');
+    newButton.innerText = "CLICK";
+
+    const newPosition = document.querySelector('div');
+    newPosition.insertAdjacentElement('afterbegin', newButton);
+}
+
+function MakeRandColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const buttons = document.querySelectorAll('button');
+
+for (let button of buttons) {
+    button.addEventListener('click', function () {
+        button.style.backgroundColor = MakeRandColor();
+    })
+}
