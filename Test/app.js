7ceb9c0edaf19f99b48552231cd2abc5fca4da34
Elske Wakeford <69579529+octothorp-elske@users.noreply.github.com>
2021-03-08 13:04:29 +0200

    Updated Navbar

--- Test/app.js
+++ Test/app.js
@@ -3,16 +3,22 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelector('.nav-links li');
+    const navLinks = document.querySelectorAll('.nav-links li');
     //Toggle Nav
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.2s ease forwards ${index / 7 + 1.0}s`;
+            }
+        });
     });
 
     //Animate Links
-    navLinks.forEach((link, index) => {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`
-    });
+
 
 }
 
